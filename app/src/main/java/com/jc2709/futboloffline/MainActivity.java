1dbd127d0f3c3a369b5fa95807a0d0f50fd7acce
Juan Medina <bastianperrogordo@gmail.com>
2026-09-07 18:33:51 -0500

    Add main activity

---
+++ app/src/main/java/com/jc2709/futboloffline/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jc2709.futboloffline;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(new GameView(this));
+    }
+}
